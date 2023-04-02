dac063d85e4ace3d9ca8d93ae933b174422e5a3c
Vic <74506415+v1ctorio@users.noreply.github.com>
2023-04-02 16:58:26 +0200

    🤡

--- src/routes/root.ts
+++
@@ -1,5 +0,0 @@
-import {FastifyRequest, FastifyReply} from 'fastify'
-
-export default async function (request: FastifyRequest, reply: FastifyReply) {
-    reply.send('GitHub social login implementation')
-}
